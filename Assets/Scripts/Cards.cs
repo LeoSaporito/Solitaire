ecfd18c5672176bed53081620b8cb56ccbd251d9
Leo21897041 <leosaporito16@gmail.com>
2026-07-01 18:05:38 -0400

    Cards can move from draw pile while following the rules of solitaire

--- Assets/Scripts/Cards.cs
+++ Assets/Scripts/Cards.cs
@@ -4,8 +4,8 @@ using System.Collections.Generic;
 
 public class Cards : MonoBehaviour
 {
-    public float xPosition;
-    public float yPosition;
+    public int xPosition;
+    public int yPosition;
 
     public float xOffset;
     public float yOffset;
@@ -13,12 +13,17 @@ public class Cards : MonoBehaviour
     public float xSpacing;
     public float ySpacing;
 
+    public string colour;
+    public string suit;
+    public int value;
+
+    public bool isSelected;
+
     public Sprite back;
     public Sprite heartAce, heartTwo, heartThree, heartFour, heartFive, heartSix, heartSeven, heartEight, heartNine, heartTen, heartJack, heartQueen, heartKing, 
                   clubAce, clubTwo, clubThree, clubFour, clubFive, clubSix, clubSeven, clubEight, clubNine, clubTen, clubJack, clubQueen, clubKing,
                   diamondAce, diamondTwo, diamondThree, diamondFour, diamondFive, diamondSix, diamondSeven, diamondEight, diamondNine, diamondTen, diamondJack, diamondQueen, diamondKing,
                   spadeAce, spadeTwo, spadeThree, spadeFour, spadeFive, spadeSix, spadeSeven, spadeEight, spadeNine, spadeTen, spadeJack, spadeQueen, spadeKing;
-
     public void SetSpacing(float x, float y)
     {
         x += xOffset;
@@ -27,12 +32,8 @@ public class Cards : MonoBehaviour
         x *= xSpacing;
         y *= ySpacing;
 
-        xPosition = x;
-        yPosition = y;
-
-        this.transform.position = new Vector2(xPosition, yPosition);        
+        this.transform.position = new Vector2(x, y);        
     }
-
     public void SetXPosition(float x)
     {
         x = xPosition;
@@ -41,7 +42,14 @@ public class Cards : MonoBehaviour
     {
         y = yPosition;
     }
-
+    public int GetXPosition()
+    {
+        return xPosition;
+    }
+    public int GetYPosition()
+    {
+        return yPosition;
+    }
     public void Reveal(int card)
     {
         SetFace(card);
@@ -50,66 +58,66 @@ public class Cards : MonoBehaviour
     {
         this.GetComponent<SpriteRenderer>().sprite = back;
     }
-    public void SetFace(int number)
+    void SetFace(int number)
     {
         switch (number)
         {
             //Hearts
-            case 0: this.GetComponent<SpriteRenderer>().sprite = heartAce; break;
-            case 1: this.GetComponent<SpriteRenderer>().sprite = heartTwo; break;
-            case 2: this.GetComponent<SpriteRenderer>().sprite = heartThree; break;
-            case 3: this.GetComponent<SpriteRenderer>().sprite = heartFour; break;
-            case 4: this.GetComponent<SpriteRenderer>().sprite = heartFive; break;
-            case 5: this.GetComponent<SpriteRenderer>().sprite = heartSix; break;
-            case 6: this.GetComponent<SpriteRenderer>().sprite = heartSeven; break;
-            case 7: this.GetComponent<SpriteRenderer>().sprite = heartEight; break;
-            case 8: this.GetComponent<SpriteRenderer>().sprite = heartNine; break;
-            case 9: this.GetComponent<SpriteRenderer>().sprite = heartTen; break;
-            case 10: this.GetComponent<SpriteRenderer>().sprite = heartJack; break;
-            case 11: this.GetComponent<SpriteRenderer>().sprite = heartQueen; break;
-            case 12: this.GetComponent<SpriteRenderer>().sprite = heartKing; break;
+            case 0: this.GetComponent<SpriteRenderer>().sprite = heartAce; this.colour = "red"; this.suit = "hearts"; this.value = 1; break;
+            case 1: this.GetComponent<SpriteRenderer>().sprite = heartTwo; this.colour = "red"; this.suit = "hearts"; this.value = 2; break;
+            case 2: this.GetComponent<SpriteRenderer>().sprite = heartThree; this.colour = "red"; this.suit = "hearts"; this.value = 3; break;
+            case 3: this.GetComponent<SpriteRenderer>().sprite = heartFour; this.colour = "red"; this.suit = "hearts"; this.value = 4; break;
+            case 4: this.GetComponent<SpriteRenderer>().sprite = heartFive; this.colour = "red"; this.suit = "hearts"; this.value = 5; break;
+            case 5: this.GetComponent<SpriteRenderer>().sprite = heartSix; this.colour = "red"; this.suit = "hearts"; this.value = 6; break;
+            case 6: this.GetComponent<SpriteRenderer>().sprite = heartSeven; this.colour = "red"; this.suit = "hearts"; this.value = 7; break;
+            case 7: this.GetComponent<SpriteRenderer>().sprite = heartEight; this.colour = "red"; this.suit = "hearts"; this.value = 8; break;
+            case 8: this.GetComponent<SpriteRenderer>().sprite = heartNine; this.colour = "red"; this.suit = "hearts"; this.value = 9; break;
+            case 9: this.GetComponent<SpriteRenderer>().sprite = heartTen; this.colour = "red"; this.suit = "hearts"; this.value = 10; break;
+            case 10: this.GetComponent<SpriteRenderer>().sprite = heartJack; this.colour = "red"; this.suit = "hearts"; this.value = 11; break;
+            case 11: this.GetComponent<SpriteRenderer>().sprite = heartQueen; this.colour = "red"; this.suit = "hearts"; this.value = 12; break;
+            case 12: this.GetComponent<SpriteRenderer>().sprite = heartKing; this.colour = "red"; this.suit = "hearts"; this.value = 13; break;
             //Clubs
-            case 13: this.GetComponent<SpriteRenderer>().sprite = clubAce; break;
-            case 14: this.GetComponent<SpriteRenderer>().sprite = clubTwo; break;
-            case 15: this.GetComponent<SpriteRenderer>().sprite = clubThree; break;
-            case 16: this.GetComponent<SpriteRenderer>().sprite = clubFour; break;
-            case 17: this.GetComponent<SpriteRenderer>().sprite = clubFive; break;
-            case 18: this.GetComponent<SpriteRenderer>().sprite = clubSix; break;
-            case 19: this.GetComponent<SpriteRenderer>().sprite = clubSeven; break;
-            case 20: this.GetComponent<SpriteRenderer>().sprite = clubEight; break;
-            case 21: this.GetComponent<SpriteRenderer>().sprite = clubNine; break;
-            case 22: this.GetComponent<SpriteRenderer>().sprite = clubTen; break;
-            case 23: this.GetComponent<SpriteRenderer>().sprite = clubJack; break;
-            case 24: this.GetComponent<SpriteRenderer>().sprite = clubQueen; break;
-            case 25: this.GetComponent<SpriteRenderer>().sprite = clubKing; break;
+            case 13: this.GetComponent<SpriteRenderer>().sprite = clubAce; this.colour = "black"; this.suit = "clubs"; this.value = 1; break;
+            case 14: this.GetComponent<SpriteRenderer>().sprite = clubTwo; this.colour = "black"; this.suit = "clubs"; this.value = 2; break;
+            case 15: this.GetComponent<SpriteRenderer>().sprite = clubThree; this.colour = "black"; this.suit = "clubs"; this.value = 3; break;
+            case 16: this.GetComponent<SpriteRenderer>().sprite = clubFour; this.colour = "black"; this.suit = "clubs"; this.value = 4; break;
+            case 17: this.GetComponent<SpriteRenderer>().sprite = clubFive; this.colour = "black"; this.suit = "clubs"; this.value = 5; break;
+            case 18: this.GetComponent<SpriteRenderer>().sprite = clubSix; this.colour = "black"; this.suit = "clubs"; this.value = 6; break;
+            case 19: this.GetComponent<SpriteRenderer>().sprite = clubSeven; this.colour = "black"; this.suit = "clubs"; this.value = 7; break;
+            case 20: this.GetComponent<SpriteRenderer>().sprite = clubEight; this.colour = "black"; this.suit = "clubs"; this.value = 8; break;
+            case 21: this.GetComponent<SpriteRenderer>().sprite = clubNine; this.colour = "black"; this.suit = "clubs"; this.value = 9; break;
+            case 22: this.GetComponent<SpriteRenderer>().sprite = clubTen; this.colour = "black"; this.suit = "clubs"; this.value = 10; break;
+            case 23: this.GetComponent<SpriteRenderer>().sprite = clubJack; this.colour = "black"; this.suit = "clubs"; this.value = 11; break;
+            case 24: this.GetComponent<SpriteRenderer>().sprite = clubQueen; this.colour = "black"; this.suit = "clubs"; this.value = 12; break;
+            case 25: this.GetComponent<SpriteRenderer>().sprite = clubKing; this.colour = "black"; this.suit = "clubs"; this.value = 13; break;
             //Diamonds
-            case 26: this.GetComponent<SpriteRenderer>().sprite = diamondAce; break;
-            case 27: this.GetComponent<SpriteRenderer>().sprite = diamondTwo; break;
-            case 28: this.GetComponent<SpriteRenderer>().sprite = diamondThree; break;
-            case 29: this.GetComponent<SpriteRenderer>().sprite = diamondFour; break;
-            case 30: this.GetComponent<SpriteRenderer>().sprite = diamondFive; break;
-            case 31: this.GetComponent<SpriteRenderer>().sprite = diamondSix; break;
-            case 32: this.GetComponent<SpriteRenderer>().sprite = diamondSeven; break;
-            case 33: this.GetComponent<SpriteRenderer>().sprite = diamondEight; break;
-            case 34: this.GetComponent<SpriteRenderer>().sprite = diamondNine; break;
-            case 35: this.GetComponent<SpriteRenderer>().sprite = diamondTen; break;
-            case 36: this.GetComponent<SpriteRenderer>().sprite = diamondJack; break;
-            case 37: this.GetComponent<SpriteRenderer>().sprite = diamondQueen; break;
-            case 38: this.GetComponent<SpriteRenderer>().sprite = diamondKing; break;
+            case 26: this.GetComponent<SpriteRenderer>().sprite = diamondAce; this.colour = "red"; this.suit = "diamonds"; this.value = 1; break;
+            case 27: this.GetComponent<SpriteRenderer>().sprite = diamondTwo; this.colour = "red"; this.suit = "diamonds"; this.value = 2; break;
+            case 28: this.GetComponent<SpriteRenderer>().sprite = diamondThree; this.colour = "red"; this.suit = "diamonds"; this.value = 3; break;
+            case 29: this.GetComponent<SpriteRenderer>().sprite = diamondFour; this.colour = "red"; this.suit = "diamonds"; this.value = 4; break;
+            case 30: this.GetComponent<SpriteRenderer>().sprite = diamondFive; this.colour = "red"; this.suit = "diamonds"; this.value = 5; break;
+            case 31: this.GetComponent<SpriteRenderer>().sprite = diamondSix; this.colour = "red"; this.suit = "diamonds"; this.value = 6; break;
+            case 32: this.GetComponent<SpriteRenderer>().sprite = diamondSeven; this.colour = "red"; this.suit = "diamonds"; this.value = 7; break;
+            case 33: this.GetComponent<SpriteRenderer>().sprite = diamondEight; this.colour = "red"; this.suit = "diamonds"; this.value = 8; break;
+            case 34: this.GetComponent<SpriteRenderer>().sprite = diamondNine; this.colour = "red"; this.suit = "diamonds"; this.value = 9; break;
+            case 35: this.GetComponent<SpriteRenderer>().sprite = diamondTen; this.colour = "red"; this.suit = "diamonds"; this.value = 10; break;
+            case 36: this.GetComponent<SpriteRenderer>().sprite = diamondJack; this.colour = "red"; this.suit = "diamonds"; this.value = 11; break;
+            case 37: this.GetComponent<SpriteRenderer>().sprite = diamondQueen; this.colour = "red"; this.suit = "diamonds"; this.value = 12; break;
+            case 38: this.GetComponent<SpriteRenderer>().sprite = diamondKing; this.colour = "red"; this.suit = "diamonds"; this.value = 13; break;
             //Spades
-            case 39: this.GetComponent<SpriteRenderer>().sprite = spadeAce; break;
-            case 40: this.GetComponent<SpriteRenderer>().sprite = spadeTwo; break;
-            case 41: this.GetComponent<SpriteRenderer>().sprite = spadeThree; break;
-            case 42: this.GetComponent<SpriteRenderer>().sprite = spadeFour; break;
-            case 43: this.GetComponent<SpriteRenderer>().sprite = spadeFive; break;
-            case 44: this.GetComponent<SpriteRenderer>().sprite = spadeSix; break;
-            case 45: this.GetComponent<SpriteRenderer>().sprite = spadeSeven; break;
-            case 46: this.GetComponent<SpriteRenderer>().sprite = spadeEight; break;
-            case 47: this.GetComponent<SpriteRenderer>().sprite = spadeNine; break;
-            case 48: this.GetComponent<SpriteRenderer>().sprite = spadeTen; break;
-            case 49: this.GetComponent<SpriteRenderer>().sprite = spadeJack; break;
-            case 50: this.GetComponent<SpriteRenderer>().sprite = spadeQueen; break;
-            case 51: this.GetComponent<SpriteRenderer>().sprite = spadeKing; break;
+            case 39: this.GetComponent<SpriteRenderer>().sprite = spadeAce; this.colour = "black"; this.suit = "spades"; this.value = 1; break;
+            case 40: this.GetComponent<SpriteRenderer>().sprite = spadeTwo; this.colour = "black"; this.suit = "spades"; this.value = 2; break;
+            case 41: this.GetComponent<SpriteRenderer>().sprite = spadeThree; this.colour = "black"; this.suit = "spades"; this.value = 3; break;
+            case 42: this.GetComponent<SpriteRenderer>().sprite = spadeFour; this.colour = "black"; this.suit = "spades"; this.value = 4; break;
+            case 43: this.GetComponent<SpriteRenderer>().sprite = spadeFive; this.colour = "black"; this.suit = "spades"; this.value = 5; break;
+            case 44: this.GetComponent<SpriteRenderer>().sprite = spadeSix; this.colour = "black"; this.suit = "spades"; this.value = 6; break;
+            case 45: this.GetComponent<SpriteRenderer>().sprite = spadeSeven; this.colour = "black"; this.suit = "spades"; this.value = 7; break;
+            case 46: this.GetComponent<SpriteRenderer>().sprite = spadeEight; this.colour = "black"; this.suit = "spades"; this.value = 8; break;
+            case 47: this.GetComponent<SpriteRenderer>().sprite = spadeNine; this.colour = "black"; this.suit = "spades"; this.value = 9; break;
+            case 48: this.GetComponent<SpriteRenderer>().sprite = spadeTen; this.colour = "black"; this.suit = "spades"; this.value = 10; break;
+            case 49: this.GetComponent<SpriteRenderer>().sprite = spadeJack; this.colour = "black"; this.suit = "spades"; this.value = 11; break;
+            case 50: this.GetComponent<SpriteRenderer>().sprite = spadeQueen; this.colour = "black"; this.suit = "spades"; this.value = 12; break;
+            case 51: this.GetComponent<SpriteRenderer>().sprite = spadeKing; this.colour = "black"; this.suit = "spades"; this.value = 13; break;
         }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,20 +5,33 @@ using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
-    public List<int> cards = new List<int>(52);
+    List<int> cards = new List<int>(52);
     public List<int> shuffledNumbers = new List<int>(52);
     public List<int> cardsInQueue = new List<int>();
+
+    List<int> hearts = new List<int>(13);
+    List<int> diamonds = new List<int>(13);
+    List<int> clubs = new List<int>(13);
+    List<int> spades = new List<int>(13);
 
     GameObject[,] gridPosition = new GameObject[7, 12];
 
     public GameObject cardPrefab;
     public GameObject drawPilePrefab;
 
+    public GameObject heartsPlacementPrefab;
+    public GameObject diamondsPlacementPrefab;
+    public GameObject clubsPlacementPrefab;
+    public GameObject spadesPlacementPrefab;
+
+    public List<GameObject> cardsSelected = new List<GameObject>(2);
+
     void Start()
     {
         ShuffleCards();
         SetupCards();
         DrawPile();
+        SuitSections();
     }
     
     void ShuffleCards()
@@ -45,7 +58,7 @@ public class GameManager : MonoBehaviour
             {
                 bool isRevealed = y == 11 - x;
 
-                SetupCardsOnStart(x, y, isRevealed);
+                GameObject cardsDrawnOnSetup = SetupCardsOnStart(x, y, isRevealed);
             }
         }
     }
@@ -74,14 +87,12 @@ public class GameManager : MonoBehaviour
             cardDrawnObj.Reveal(face);
         }
 
-        print(face);
-
         return cardDrawn;
     }
 
     void DrawPile()
     {
-        GameObject drawPileObj = Instantiate(drawPilePrefab, new Vector2(-7.5f, 2.25f), Quaternion.identity);
+        GameObject drawPileObj = Instantiate(drawPilePrefab, new Vector2(-8, 3), Quaternion.identity);
     }
     GameObject DrawCard(float x, float y)
     {
@@ -98,6 +109,15 @@ public class GameManager : MonoBehaviour
 
         return cardDrawn;
     }
+    
+    void SuitSections()
+    {
+        Instantiate(heartsPlacementPrefab, new Vector2(8, 3), Quaternion.identity);
+        Instantiate(diamondsPlacementPrefab, new Vector2(8, 1), Quaternion.identity);
+        Instantiate(clubsPlacementPrefab, new Vector2(8, -1), Quaternion.identity);
+        Instantiate(spadesPlacementPrefab, new Vector2(8, -3), Quaternion.identity);
+    }
+    
     public void OnClick(InputValue value)
     {
         if (value.isPressed)
@@ -110,10 +130,42 @@ public class GameManager : MonoBehaviour
             {
                 if (hit.collider.CompareTag("Draw Pile"))
                 {
-                    DrawCard(-7.5f, 0f);
+                    DrawCard(-8, 1);
                 }
-                Cards cardObj = hit.collider.GetComponent<Cards>();
+                if (hit.collider.CompareTag("Card Drawn"))
+                {
+                    Cards cardObjScript = hit.collider.GetComponent<Cards>();
+
+                    cardObjScript.isSelected = true;
+
+                    cardsSelected.Add(hit.collider.gameObject);
+                }
             }
         }
+    }
+    void CardCheck()
+    {
+        if (cardsSelected.Count == 2)
+        {
+            Cards cardOneScript = cardsSelected[0].GetComponent<Cards>();
+            Cards cardTwoScript = cardsSelected[1].GetComponent<Cards>();
+
+            if (cardOneScript.colour != cardTwoScript.colour && cardOneScript.value == cardTwoScript.value - 1)
+            {
+                cardOneScript.transform.position = new Vector2(cardTwoScript.transform.position.x, cardTwoScript.transform.position.y - 1);
+                gridPosition[cardTwoScript.GetXPosition(),cardTwoScript.GetYPosition()] = cardsSelected[0];
+                cardsSelected[0].GetComponent<SpriteRenderer>().sortingOrder = cardsSelected[1].GetComponent<SpriteRenderer>().sortingOrder + 1;
+
+                cardOneScript.isSelected = false;
+                cardTwoScript.isSelected = false;
+            }
+            
+            cardsSelected.RemoveAt(1);
+            cardsSelected.RemoveAt(0);
+        }
+    }
+    private void Update()
+    {
+        CardCheck();
     }
 }
